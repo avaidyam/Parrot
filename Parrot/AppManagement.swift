1e3a373b58d76663e2c3c8b4fa44c20f94097e91
Aditya Vaidyam <avaidyam@purdue.edu>
2016-06-15 17:22:49 -0400

    Try to leverage GitHub for AppManagement...

---
+++ Parrot/AppManagement.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let __url = "https://api.github.com/repos/avaidyam/Parrot/releases"
+// dat[0]["zipball_url"] = app update
+// dat[0]["body"] = release notes
+
+public func checkLatestRelease() {
+	
+}
